325db96dd0cf4e6f7959f1d9fa6fd1f64db3824d
Ricky Dharmawan <rickydharmawan1@gmail.com>
2024-07-07 20:17:32 +0700

    tes js buat login local

--- resource/login.js
+++ resource/login.js
@@ -1,14 +1,16 @@
-// add event listener to form submit
-document.querySelector('form').addEventListener('submit', (event) => {
-    event.preventDefault(); // prevent form from submitting
-  
-    const Username = document.querySelector('#Username').value;
-    const Password = document.querySelector('#Password').value;
-  
-    // check if Username and Password are correct
-    if (Username === 'myusername' && Password === 'mypassword') {
-      alert('Login successful!');
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "123") {
+        alert("You have successfully logged in.");
+        location.reload();
     } else {
-      alert('Incorrect Username or Password.');
+        loginErrorMsg.style.opacity = 1;
     }
-  });
+})
