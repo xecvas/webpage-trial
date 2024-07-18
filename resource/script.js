30b159f1e66171266fe6cdaa4654db22778c28fa
Ricky Dharmawan <rickydharmawan1@gmail.com>
2024-07-18 14:39:12 +0700

    add local saving for dark mode toggle

--- resource/script.js
+++ resource/script.js
@@ -9,3 +9,28 @@ $(document).ready(function() {
         $('form-text').toggleClass('dark');
     });
 });
+
+// Save toggle setting to localStorage
+function saveToggleSetting() {
+  const checkbox = document.getElementById("checkbox");
+  const isChecked = checkbox.checked;
+  localStorage.setItem("isChecked", JSON.stringify(isChecked));
+}
+
+// Load toggle setting from localStorage
+function loadToggleSetting() {
+  const checkbox = document.getElementById("checkbox");
+  const isChecked = JSON.parse(localStorage.getItem("isChecked"));
+  if (isChecked) {
+    checkbox.checked = isChecked;
+    $('body').addClass('dark');
+    $('canvas').addClass('dark');
+    $('form-text').addClass('dark');
+  }
+}
+
+$(document).ready(function() {
+  loadToggleSetting();
+  saveToggleSetting();
+  $("#checkbox").change(saveToggleSetting);
+});
