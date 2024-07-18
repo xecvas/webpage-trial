7cf2fa7c1dccd45b75cfe4d0251c6af8d5dff82a
Ricky Dharmawan <rickydharmawan1@gmail.com>
2024-07-18 14:39:55 +0700

    fix captcha error

--- resource/captcha.js
+++ resource/captcha.js
@@ -4,11 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const captchaInput = document.getElementById("captchaInput");
   const captchaError = document.getElementById("captchaError");
   const refreshCaptchaButton = document.getElementById("refreshCaptcha");
-  
+
   let captchaText = "";
 
   function generateCaptcha() {
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const chars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     captchaText = "";
     for (let i = 0; i < 6; i++) {
       captchaText += chars.charAt(Math.floor(Math.random() * chars.length));
@@ -22,19 +23,21 @@ document.addEventListener("DOMContentLoaded", function () {
     captchaContext.strokeText(captchaText, centerX, centerY);
   }
 
-  document.getElementById("login-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    if (captchaInput.value === captchaText) {
-      alert("CAPTCHA verified successfully!");
-      captchaError.style.display = "none";
-      captchaInput.value = "";
-      generateCaptcha(); // Generate a new CAPTCHA after successful verification
-    } else {
-      captchaError.style.display = "block";
-      captchaInput.value = "";
-      generateCaptcha();
-    }
-  });
+  document
+    .getElementById("login-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      if (captchaInput.value === captchaText) {
+        alert("CAPTCHA verified successfully!");
+        captchaError.style.display = "none";
+        captchaInput.value = "";
+        generateCaptcha(); // Generate a new CAPTCHA after successful verification
+      } else {
+        captchaError.style.display = "block";
+        captchaInput.value = "";
+        generateCaptcha();
+      }
+    });
 
   refreshCaptchaButton.addEventListener("click", function () {
     generateCaptcha();
