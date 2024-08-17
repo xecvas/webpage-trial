a71fe27cfa6ed4bb57ebf9b5ad507cc1e11f59af
rickydharmawan1 <rickydharmawan1@gmail.com>
2024-08-17 05:38:50 +0000

    test route

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+import os
+
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  return "<p>Hello, World!</p>"
+@app.route("/test")
+def index():
+  print(path)
+  return send_file('test.html')
+
+def main():
+    app.run(port=int(os.environ.get('PORT', 800)))
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    main()
